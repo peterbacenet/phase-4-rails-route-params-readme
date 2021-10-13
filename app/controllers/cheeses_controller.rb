cde3ff54887d2c351761e5c915b60085fdc1e7af
Peter Bacenet <peterbacenet@icloud.com>
2021-10-12 22:41:15 -0400

    Assignment Complete

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,10 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    # byebug
+    selected_cheese = Cheese.all.find_by(id: params[:id])
+    render json: selected_cheese
+  end
 
 end
